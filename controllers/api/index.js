fd5a5de90aaae2181abc553293b8c415d0c4df9b
Jason <jasonkodama@gmail.com>
2023-06-05 14:01:39 -0600

    added chatRoutes and modified other routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 const postRoutes = require('./postRoutes.js')
 const aiRoutes = require('./aiRoutes.js')
+const userRoutes = require('./userRoutes.js')
+const chatRoutes = require()
 
 router.use('/projects', postRoutes)
 router.use('/chatGpt', aiRoutes)
+router.use('/users', userRoutes);
 
 
 module.exports = router
