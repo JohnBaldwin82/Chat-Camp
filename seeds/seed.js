21237a78b09c32d8071b0220619945d3c731e311
Jason <jasonkodama@gmail.com>
2023-06-06 00:02:54 -0600

    fixed login issue

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,26 @@
-const userSeeds = require('./userSeeds');
-const postSeeds = require('./postSeeds');
-const chatSeeds = require('./chatSeeds');
-
+// const userSeeds = require('./userSeeds');
+// const postSeeds = require('./postSeeds');
+// const chatSeeds = require('./chatSeeds');
+const { User, Post, Chat } = require('../models')
 const sequelize = require('../config/connection');
 
-const seedAll = async () => {
-    await sequelize.sync({ force: true });
-    await userSeeds();
-    await postSeeds();
-    await chatSeeds();
+const userSeeds = require('./userSeeds.json');
+const postSeeds = require('./postSeeds.json');
+const chatSeeds = require('./chatSeeds.json');
 
-    process.exit(0);
+const seedAll = async () => {
+    try {
+        await sequelize.sync({ force: true})
+        await User.bulkCreate(userSeeds, {
+            individualHooks: true
+        });
+        await Post.bulkCreate(postSeeds);
+        await Chat.bulkCreate(chatSeeds);
+
+        process.exit(0);
+    } catch(err) {
+        console.log(err)
+    }
 };
 
 seedAll();
