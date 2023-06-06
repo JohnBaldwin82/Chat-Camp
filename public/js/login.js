72fe0bb30a93b222c4f1f678328d42446a110e59
John Baldwin <johnbaldwin82@gmail.com>
2023-06-05 21:02:13 -0600

    login code

--- public/js/login.js
+++ public/js/login.js
@@ -1,43 +1,42 @@
 //form async preventDefault function that gets values from login
 //if statement to check for email and password then sends post request to api
-const formHandlerLogin = (e) => {
-// e.preventDefault();
-const pwd = document.getElementById('#loginPwd').value.trim();
-const email = document.getElementById('#emailId').value.trim();
-const personName = document.getElementById('#personName').value.trim();
-console.log('is it working?')
-if(email && pwd) {
-    // const response = await fetch('/api/user/login', {
-    //     method:'POST',
-    //     body:JSON.stringify({email, pwd}),
-    //     headers: {'Content-Type': 'application/json'},
-    // });
-if(response.ok){
-    document.location.replace('/profile');
-}else{
-    alert(response.statusText);
-}
-}
+const formHandlerLogin = async (e) => {
+  // e.preventDefault();
+  const pwd = document.getElementById("loginPwd").value.trim();
+  const email = document.getElementById("loginEmailId").value.trim();
+  // const personName = document.getElementById('personName').value.trim();
+  console.log("is it working?");
+  if (email && pwd) {
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({ email, pwd }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
 };
 const formHandlersignup = async (e) => {
-    e.preventDefault();
-    const pwd = document.getElementById('#loginPwd').value.trim();
-    const email = document.getElementById('#emailId').value.trim();
-    const userName = document.getElementById('#user_Name').value.trim();
-    if(email && pwd && personName) {
-        const response = await fetch('/api/user', {
-            method:'POST',
-            body:JSON.stringify({email, pwd, userName}),
-            headers: {'Content-Type': 'application/json'},
-        });
-    if(response.ok){
-        document.location.replace('/profile');
-    }else{
-        alert(response.statusText);
+  e.preventDefault();
+  const pwd = document.getElementById("loginPwd").value.trim();
+  const email = document.getElementById("loginEmailId").value.trim();
+  const userName = document.getElementById("user_Name").value.trim();
+  if (email && pwd && personName) {
+    const response = await fetch("/api/user", {
+      method: "POST",
+      body: JSON.stringify({ email, pwd, userName }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
     }
-    }
-    };
+  }
+};
 
-    document.querySelector('#signForm').addEventListener('click', this.formHandlerLogin);
-    document.querySelector('#signUp').addEventListener('submit', formHandlersignup);
-    
+document.querySelector("#signForm").addEventListener("click", formHandlerLogin);
+// document.querySelector('#signUp').addEventListener('submit', this.formHandlersignup);
